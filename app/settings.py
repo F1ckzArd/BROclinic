c638f5e1028a60d928d7d216624b04c89cc14667
Ваше Имя <_email@example.com>
2024-11-18 17:00:51 +0600

    deploy

--- app/settings.py
+++ app/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+ALLOWED_HOSTS = ['F1ckzArd.pythonanywhere.com']
 
 
 ALLOWED_HOSTS = ['.herokuapp.com']
